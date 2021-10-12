4a827496e16f30a8b6a09e9f1a0276b0c0ca40ce
Rimantas <belauzas@yahoo.com>
2021-10-12 19:53:51 +0300

    Funkciju dokumentacija

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const data = require('./lib/data.js');
 
 console.log(data);
 
-data.create()
-data.read();
+data.create('users', 'petras', { name: 'Petras', age: 99 });
+data.read('users', 'petras');
 data.update();
 data.delete();
+
+data.create('books', 'du-gaideliai', { text: 'asd' });
+data.read('books', 'du-gaideliai');
