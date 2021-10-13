9d518e6acf7f0a3cbd588bbeba8eec692692f8d1
Rimantas <belauzas@yahoo.com>
2021-10-13 19:27:11 +0300

    File update

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
 const data = require('./lib/data.js');
 
-// data.create('users', 'petras', { name: 'Petras', age: 99 }, (state, msg) => {
-//     console.log(state, msg);
+const initialDataPetras = { name: 'Petras', age: 99 };
+// data.create('users', 'petras', initialDataPetras, (err, msg) => {
+//     console.log(err, msg);
 //     console.log('Sekantys zingsniai po bandymo sukurti faila....');
 // });
 
-// data.create('books', 'du-gaideliai', { text: 'Baltus zirnius kule' }, (state, msg) => {
-//     console.log(state, msg);
+// data.create('books', 'du-gaideliai', { text: 'Baltus zirnius kule' }, (err, msg) => {
+//     console.log(err, msg);
 //     console.log('Sekantys zingsniai po bandymo sukurti faila....');
 // });
 
+// pirminis perskaitymas
 data.read('users', 'petras', (err, content) => {
     const fileData = JSON.parse(content);
-    console.log(fileData.name);
-    console.log(fileData.age);
+    console.log('READ 1:', fileData);
+
+    // prie Petro objekto prideti megsta spalva: red
+    data.update('users', 'petras', { ...initialDataPetras, favoriteColor: 'red' }, (err, msg) => {
+        console.log('UPDATE 1:', err, msg);
+
+        // dar karta perskaitom
+        data.read('users', 'petras', (err, content) => {
+            const fileData = JSON.parse(content);
+            console.log('READ 2:', fileData);
+        })
+    })
 })
+
